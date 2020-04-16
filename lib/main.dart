602cab91abeff8ead4dbaca689e772a7c341fd87
Sparrow Paul <sparrowpaul2@gmail.com>
2020-04-16 08:16:07 +0000

    a simple app to predict random numbers

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,54 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text(
+          'Ask me anything',
+        ),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        highlightColor: Colors.transparent,
+        onPressed: () {
+          setState(() {
+            changeBall();
+          });
+        },
+        child: Image(
+          image: AssetImage('images/ball$ballNumber.png'),
+        ),
+      ),
+    );
+  }
+
+  void changeBall() {
+    ballNumber = Random().nextInt(5) + 1;
+  }
+}
